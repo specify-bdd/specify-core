f058982497ee774d0f3b69442d8bf8b10823b15c
Adam Lacoste <adam.lacoste@hotmail.com>
2025-05-29 16:51:51 -0500

    [#24] Removed a commented out import statement that is no longer necessary

--- runner-plugin-cli/cucumber/support/param_types.ts
+++ runner-plugin-cli/cucumber/support/param_types.ts
@@ -7,7 +7,6 @@
 
 import { FileParam } from "@/types/params";
 import { defineParameterType } from "@cucumber/cucumber";
-// import { lookup } from "specify-quick-ref";
 
 import * as path from "node:path";
 
