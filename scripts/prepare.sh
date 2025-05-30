f256a208940bcb9d16463cba8b3a6acf9e024188
MentalFusion <michael.lamar.b@gmail.com>
2025-05-29 21:19:05 -0400

    [39] Updated the prepare script to use exits if any build command fails so we can catch them early.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # build workspaces in sequential order to avoid race conditions on dependencies
-pnpm -r --filter ./quick-ref run build
-pnpm -r --filter ./runner run build
-pnpm -r --filter ./runner-plugin-cli run build
+pnpm -r --filter ./quick-ref run build || exit 1
+pnpm -r --filter ./runner run build || exit 1
+pnpm -r --filter ./runner-plugin-cli run build || exit 1
