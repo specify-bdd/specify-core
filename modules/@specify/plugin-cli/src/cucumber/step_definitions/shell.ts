50dcbacb04306b5fa3f7c21373010e41f0eb05bd
Wild Love <wild.love.swe@proton.me>
2025-06-10 15:59:01 -0500

    [#6][#7][#8] Rename "classes" dir to"lib" .

--- modules/@specify/plugin-cli/src/cucumber/step_definitions/shell.ts
+++ modules/@specify/plugin-cli/src/cucumber/step_definitions/shell.ts
@@ -7,8 +7,8 @@
 
 import assert, { AssertionError } from "node:assert/strict";
 import { Given, Then, When } from "@cucumber/cucumber";
-import { Commander } from "@/classes/Commander";
-import { ShellSession } from "@/classes/ShellSession";
+import { Commander } from "@/lib/Commander";
+import { ShellSession } from "@/lib/ShellSession";
 
 Given("that a command line prompt is available", setupCLI);
 
