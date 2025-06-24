87b7d1e0e46095f105ee704ac1c1de558c53d932
MentalFusion <michael.lamar.b@gmail.com>
2025-06-24 12:42:06 -0400

    [#45] Created a new lint rule for alinging imports with our pedantic rules. Linted the align assignments rule and fixed TypeScript strict mode issues therein.

--- modules/@specify/eslint-plugin/main.ts
+++ modules/@specify/eslint-plugin/main.ts
@@ -1,9 +1,11 @@
 import alignAssigments from "./rules/align-assignments";
+import alignImports    from "./rules/align-imports";
 
 export default {
     "extends": ["plugin:eslint/recommended"],
     "rules": {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
+        /* eslint-disable @typescript-eslint/naming-convention */
         "align-assignments": alignAssigments,
+        "align-imports": alignImports,
     },
 };
