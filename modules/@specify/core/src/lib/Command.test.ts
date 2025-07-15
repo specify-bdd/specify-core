9fee43e6a3e329fc089791d9d7d54c5920f3c870
Wild Love <wild.love.swe@proton.me>
2025-07-15 15:30:59 -0500

    [#6][#35] Re-add the abstract class testing, but make it concrete.

---
+++ modules/@specify/core/src/lib/Command.test.ts
@@ -0,0 +1,41 @@
+import {
+    Command,
+    COMMAND_DEFAULT_OPTS
+} from "./Command";
+
+class ConcreteCommand extends Command {};
+
+describe("Command", () => {
+    const emptyArgs = { "_": [] };
+    const emptyOpts = {};
+
+    describe("constructor()", () => {
+        describe("iniitializes options...", () => {
+            it("...with defaults when no user options are provided", () => {
+                const cmd = new ConcreteCommand(emptyOpts);
+
+                expect(cmd.debug).toBe(COMMAND_DEFAULT_OPTS.debug);
+                expect(cmd.logPath).toBe(COMMAND_DEFAULT_OPTS.logPath);
+            });
+
+            it("...with merged values when user options are provided", () => {
+                const userOpts = { "debug": true };
+                const cmd      = new ConcreteCommand(userOpts);
+
+                expect(cmd.debug).toBe(userOpts.debug);
+            });
+        });
+    });
+
+    describe("execute()", () => {
+        it("returns an error result when called", async () => {
+            const cmd = new ConcreteCommand(emptyOpts);
+            const res = await cmd.execute(emptyArgs);
+
+            expect(res.ok).toBe(false);
+            expect(res.status).toBe(2);
+            expect(res.error).toBeTruthy();
+            expect(res.result).toBeUndefined();
+        });
+    });
+});
