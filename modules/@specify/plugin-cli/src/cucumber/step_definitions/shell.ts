0d06ec871ad9c569409f09ed10c1b7fd4be058df
Adam Lacoste <adam.lacoste@hotmail.com>
2025-08-01 12:13:32 -0500

    [#82] committing a draft of my step def permutations which I expect to revise after integrating Michael's recent changes

--- modules/@specify/plugin-cli/src/cucumber/step_definitions/shell.ts
+++ modules/@specify/plugin-cli/src/cucumber/step_definitions/shell.ts
@@ -8,8 +8,8 @@
 import { Given, Then, When      } from "@cucumber/cucumber";
 import assert, { AssertionError } from "node:assert/strict";
 
-import { SessionManager } from "@/lib/SessionManager";
-import { ShellSession   } from "@/lib/ShellSession";
+import { SessionManager, IOStream } from "@/lib/SessionManager";
+import { ShellSession             } from "@/lib/ShellSession";
 
 Given("a/another CLI shell", startDefaultShell);
 
@@ -20,6 +20,92 @@ When("a/the user runs the command/process {string}", { "timeout": 60000 }, execC
 When("a/the user starts the (async )command/process {string}", execCommand);
 
 When("a/the user waits for the last command to return", { "timeout": 60000 }, waitForCommandReturn);
+
+When("a/the user waits for terminal output", { "timeout": 60000 }, waitForAnyCommandOutput);
+
+When(
+    "a/the user waits for terminal output on STDERR",
+    { "timeout": 60000 },
+    waitForCommandOutputOnSTDERR,
+);
+
+When(
+    "a/the user waits for terminal output on STDOUT",
+    { "timeout": 60000 },
+    waitForCommandOutputOnSTDOUT,
+);
+
+When(
+    "a/the user waits for terminal output on STDERR to be {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForExactCommandOutputOnSTDERR,
+);
+
+When(
+    "a/the user waits for terminal output on STDOUT to be {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForExactCommandOutputOnSTDOUT,
+);
+
+When(
+    "a/the user waits for terminal output on STDERR to be {string}",
+    { "timeout": 60000 },
+    waitForExactCommandOutputOnSTDERR,
+);
+
+When(
+    "a/the user waits for terminal output on STDOUT to be {string}",
+    { "timeout": 60000 },
+    waitForExactCommandOutputOnSTDOUT,
+);
+
+When(
+    "a/the user waits for terminal output on STDERR to include {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutputOnSTDERR,
+);
+
+When(
+    "a/the user waits for terminal output on STDOUT to include {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutputOnSTDOUT,
+);
+
+When(
+    "a/the user waits for terminal output on STDERR to include {string}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutputOnSTDERR,
+);
+
+When(
+    "a/the user waits for terminal output on STDOUT to include {string}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutputOnSTDOUT,
+);
+
+When(
+    "a/the user waits for terminal output to be {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForExactCommandOutput,
+);
+
+When(
+    "a/the user waits for terminal output to be {string}",
+    { "timeout": 60000 },
+    waitForExactCommandOutput,
+);
+
+When(
+    "a/the user waits for terminal output to include {ref:consoleOutput}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutput,
+);
+
+When(
+    "a/the user waits for terminal output to include {string}",
+    { "timeout": 60000 },
+    waitForSimilarCommandOutput,
+);
 
 Then("the command should return a/an/the {ref:exitCode} exit code", verifyExitCode);
 
@@ -110,4 +196,90 @@ function verifyExitCode(exitCode: number): void {
 async function waitForCommandReturn(): Promise<void> {
     assert.ok(this.cli.manager);
     await this.cli.manager.waitForReturn();
+}
+
+/**
+ * Wait for the last command to send some output.
+ *
+ * @param stream  - The stream to watch for output
+ * @param pattern - The pattern to match output against
+ */
+async function waitForCommandOutput(stream?: IOStream, pattern?: string): Promise<void> {
+    assert.ok(this.cli.manager);
+    await this.cli.manager.waitForOutput({ pattern, stream });
+}
+
+/**
+ * Wait for literally any output.
+ */
+async function waitForAnyCommandOutput(): Promise<void> {
+    await waitForCommandOutput.call(this);
+}
+
+/**
+ * Wait for output on the STDERR stream.
+ */
+async function waitForCommandOutputOnSTDERR(): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDERR);
+}
+
+/**
+ * Wait for output on the STDOUTstream.
+ */
+async function waitForCommandOutputOnSTDOUT(): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDOUT);
+}
+
+/**
+ * Wait for output exactly matching a specific phrase.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForExactCommandOutput(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.ANY, `^${pattern}$`);
+}
+
+/**
+ * Wait for output exactly matching a specific phrase on the STDERR stream.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForExactCommandOutputOnSTDERR(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDERR, `^${pattern}$`);
+}
+
+/**
+ * Wait for output exactly matching a specific phrase on the STDOUT stream.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForExactCommandOutputOnSTDOUT(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDOUT, `^${pattern}$`);
+}
+
+/**
+ * Wait for output partially matching a specific phrase.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForSimilarCommandOutput(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.ANY, pattern);
+}
+
+/**
+ * Wait for output partially matching a specific phrase on the STDERR stream.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForSimilarCommandOutputOnSTDERR(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDERR, pattern);
+}
+
+/**
+ * Wait for output partially matching a specific phrase on the STDOUT stream.
+ *
+ * @param pattern - The pattern to match output against
+ */
+async function waitForSimilarCommandOutputOnSTDOUT(pattern: string): Promise<void> {
+    await waitForCommandOutput.call(this, IOStream.STDOUT, pattern);
 }
