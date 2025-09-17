3cd2199c241f5cece5af52b42d2b0e301eb83246
Adam Lacoste <adam.lacoste@hotmail.com>
2025-09-17 15:00:25 -0500

    [#133] You would think I would know the option for pushing tags by now but apparently I do not

--- scripts/github/commit_tag.sh
+++ scripts/github/commit_tag.sh
@@ -32,6 +32,6 @@ git commit -a -m "$COMMIT_MSG" || fail "Couldn't commit changes to Git repo."
 
 git tag -a -m "$COMMIT_MSG" $GIT_TAG || fail "Couldn't tag the latest commit."
 
-git push -t $GIT_REMOTE $GIT_BRANCH || fail "Couldn't push changes to $GIT_REMOTE."
+git push --tags $GIT_REMOTE $GIT_BRANCH || fail "Couldn't push changes to $GIT_REMOTE."
 
 end
