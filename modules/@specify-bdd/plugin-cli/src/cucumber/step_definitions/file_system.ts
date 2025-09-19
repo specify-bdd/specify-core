6570456098b36872e7ce07d4b1d58e32fe2a89fd
Wild Love <wild.love.swe@proton.me>
2025-09-19 17:08:26 -0600

    [#133][#182] Move new step defs to the file-system step def file and give them docblocks.

--- modules/@specify-bdd/plugin-cli/src/cucumber/step_definitions/file_system.ts
+++ modules/@specify-bdd/plugin-cli/src/cucumber/step_definitions/file_system.ts
@@ -4,12 +4,20 @@
  * Cucumber step definitions covering interactions with a file system.
  */
 
-import { Given, When            } from "@cucumber/cucumber";
+import { Given, Then, When            } from "@cucumber/cucumber";
 import assert, { AssertionError } from "node:assert/strict";
+import { existsSync        } from "node:fs";
+import { mkdtemp, readFile } from "node:fs/promises";
+import { tmpdir            } from "node:os";
+import { join              } from "node:path";
 
+Given("a new temp file path referenced as {string}", createTempFileRef);
 Given("(that )the working directory is {filePath}", changeDirectory);
 
 When("a/the user changes the working directory to {filePath}", changeDirectory);
+
+Then("the {ref} file content should be empty", verifyFileIsEmpty);
+Then("the {ref} file path should exist", verifyFilePathExists);
 
 /**
  * Change the current working directory in the active shell.
@@ -30,4 +38,53 @@ async function changeDirectory(dirPath: string): Promise<void> {
     await this.cli.manager.waitForReturn();
 
     assert.equal(this.cli.manager.exitCode, 0, `Could not change directory to ${dirPath}.`);
+}
+
+/**
+ * Create a new /tmp filepath and store it in QuickRef at the given address.
+ * 
+ * @param address - The dot notation address at which to store the temp file path
+ */
+async function createTempFileRef(address: string): Promise<void> {
+    const dirPath  = await mkdtemp(join(tmpdir(), "specify", "test-"));
+    const filePath = join(dirPath, "rerun.txt");
+
+    this.quickRef.setRefByAddress(address, filePath);
+}
+
+/**
+ * Verify that the file at the given path is empty.
+ * 
+ * @param filePath - The file path to check for emptiness
+ * 
+ * @throws AssertionError
+ * If the file does not exist.
+ * 
+ * @throws AssertionError
+ * If the file is not empty.
+ */
+async function verifyFileIsEmpty(filePath: string): Promise<void> {
+    await verifyFilePathExists(filePath);
+
+    const content = await readFile(filePath, "utf8");
+
+    assert.ok(
+        !content,
+        new AssertionError({ "message": "Expected file to be empty but it is not." }),
+    );
+}
+
+/**
+ * Verify that a file exists at the given path.
+ * 
+ * @param filePath - The file path to check for existence
+ * 
+ * @throws AssertionError
+ * If the file does not exist.
+ */
+async function verifyFilePathExists(filePath: string): Promise<void> {
+    assert.ok(
+        existsSync(filePath),
+        new AssertionError({ "message": `The file path "${filePath}" does not exist` }),
+    );
 }
