f1faf012a9b74f836e3f077f26ec1dfaf14fc259
Adam Lacoste <adam.lacoste@hotmail.com>
2025-12-04 15:27:53 -0600

    [#224] Created new CucumberManager class and accompanying unit test

---
+++ modules/@specify-bdd/specify/src/lib/CucumberManager.ts
@@ -0,0 +1,46 @@
+import Cucumber from "@cucumber/cucumber";
+
+export interface CucumberLike {
+    Given: CucumberStepDefLike;
+    Then: CucumberStepDefLike;
+    When: CucumberStepDefLike;
+}
+
+interface CucumberStepDefLike {
+    (pattern: string, options?: object, fn: () => void): void;
+}
+
+let instance: CucumberManager | null = null;
+
+/**
+ * A wrapper for Cucumber which streamlines, enhances, and enforces consistency
+ * of Cucumber usage throughout Specify.
+ */
+export class CucumberManager {
+    /**
+     * The managed Cucumber object.
+     */
+    cucumber: CucumberLike;
+
+    /**
+     * Accept a Cucumber object and store it for easy access.
+     *
+     * @param cucumber - The Cucumber object to manage
+     */
+    constructor(cucumber: CucumberLike) {
+        this.cucumber = cucumber;
+    }
+
+    /**
+     * Get the singleton instance of CucumberManager.
+     *
+     * @returns The CucumberManager instance
+     */
+    static getInstance(): CucumberManager {
+        if (instance) {
+            return instance;
+        }
+
+        return (instance = new CucumberManager(Cucumber));
+    }
+}
