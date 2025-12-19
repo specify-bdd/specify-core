955cefc030ab8d9f080e1f9cc10d970fed17094b
Adam Lacoste <adam.lacoste@hotmail.com>
2025-12-19 12:50:20 -0600

    [#226] Added integration test for index module

---
+++ modules/@specify-bdd/specify/src/lib/tests/integration/index.test.ts
@@ -0,0 +1,23 @@
+import { defineStep } from "../../../index";
+
+const { mockGiven } = vi.hoisted(() => {
+    return { "mockGiven": vi.fn() };
+});
+
+vi.mock("@cucumber/cucumber", () => {
+    let cucumber = { "Given": mockGiven };
+
+    return { "default": cucumber, ...cucumber };
+});
+
+describe("entrypoint module (integration)", () => {
+    describe("exports", () => {
+        describe("defineStep()", () => {
+            it("registers a step definition with Cucumber", () => {
+                defineStep("Given a step definition", () => true);
+
+                expect(mockGiven).toHaveBeenCalled();
+            });
+        });
+    });
+});
