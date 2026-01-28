9117160879096cdec3b518f582cb65b13930ad32
Adam Lacoste <adam.lacoste@hotmail.com>
2026-01-28 13:14:43 -0600

    [#255] Moved SpecifyWorld class into its own module in lib and added a unit test for it

---
+++ modules/@specify-bdd/specify/src/lib/tests/SpecifyWorld.test.ts
@@ -0,0 +1,55 @@
+import { World    } from "@cucumber/cucumber";
+import { QuickRef } from "@specify-bdd/quick-ref";
+
+import { SpecifyWorld } from "@/lib/SpecifyWorld";
+
+const { FakeWorld } = vi.hoisted(() => {
+    return { "FakeWorld": vi.fn() };
+});
+
+vi.mock("@cucumber/cucumber", () => {
+    const cucumber = {
+        "World": FakeWorld,
+    };
+
+    return { "default": cucumber, ...cucumber };
+});
+
+vi.mock("@specify-bdd/quick-ref");
+
+describe("Specify World", () => {
+    const opts = {};
+
+    let world: SpecifyWorld;
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+
+        world = new SpecifyWorld(opts);
+    });
+
+    describe("constructor", () => {
+        it("passes options through to the Cucumber World constructor", () => {
+            expect(World).toHaveBeenCalledTimes(1);
+            expect(World).toHaveBeenCalledWith(opts);
+        });
+
+        it("initializes a new QuickRef instance", () => {
+            expect(QuickRef).toHaveBeenCalledTimes(1);
+            expect(QuickRef).toHaveBeenCalledWith();
+        });
+    });
+
+    describe("properties", () => {
+        describe("quickRef", () => {
+            it("returns the stored QuickRef instance", () => {
+                expect(world.quickRef).toBeInstanceOf(QuickRef);
+            });
+
+            it("is read-only", () => {
+                expect(() => world.quickRef = new QuickRef())
+                    .toThrow("Cannot set property quickRef");
+            });
+        });
+    })
+});
