5c240e37783302fd6f16ac31cf42dd2d38c7b422
Adam Lacoste <adam.lacoste@hotmail.com>
2026-03-02 15:38:15 -0600

    [#275] Added a refsPath world param, defined in the outer context of Specify, which will help us locate the refs file from the inner context of Cucumber, which might not share the same CWD

--- modules/@specify-bdd/specify/src/config/cucumber.config.ts
+++ modules/@specify-bdd/specify/src/config/cucumber.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 import type { IConfiguration } from "@cucumber/cucumber/api";
 
 export type CucumberConfig = Partial<IConfiguration>;
@@ -7,6 +9,8 @@ export const cucumber: CucumberConfig = {
     "retryTagFilter":  "@flaky or @retry",
     "tags":            "not @skip",
     "worldParameters": {
+        // The path of the refs file relative to the project root
+        "refsPath": path.resolve("./specify.refs.json"),
         // similar value as to what you'd see if you ran $PATH in your terminal,
         // but there is also some node-specific path info added to it.
         "userPath": process.env.PATH,
