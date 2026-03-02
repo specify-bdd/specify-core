1b590f25308b96608af16b01eee8751e00f86b80
Adam Lacoste <adam.lacoste@hotmail.com>
2026-03-02 15:35:43 -0600

    [#275] Removed the refs path config because it isn't used anywhere and only served as a confusing red herring while investigating this issue

--- modules/@specify-bdd/specify/src/config/paths.config.ts
+++ modules/@specify-bdd/specify/src/config/paths.config.ts
@@ -4,13 +4,11 @@ export type PathsConfig = {
     [key: string]: string;
     gherkin: string;
     logs: string;
-    refs: string;
 };
 
 export const paths: PathsConfig = {
     "gherkin": "./features/**/*.feature",
     "logs":    "./logs/specify",
-    "refs":    "./specify.refs.json",
     "rerun":   `${os.tmpdir()}/specify/rerun.txt`,
     "working": ".",
 };
